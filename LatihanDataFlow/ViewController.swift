3ddaa601b13a2cf1e35bf89cc3a0a74b09b5a380
Reynard Vincent Nata <reynardvnata@yahoo.co.id>
2019-05-16 11:33:53 +0700

    Adding Data Flow

--- LatihanDataFlow/ViewController.swift
+++ LatihanDataFlow/ViewController.swift
@@ -9,12 +9,44 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var fullNameTextField: UITextField!
+    @IBOutlet weak var phoneTextField: UITextField!
+    
+    @IBAction func registerPressed(_ sender: Any) {
+        if fullNameTextField.text == "" || phoneTextField.text == ""{
+            let alert = UIAlertController(title: "Error", message: "All textfill must be filled", preferredStyle: .alert)
+            let action = UIAlertAction(title: "OK", style: .default) { (alertAction) in
+                alert.dismiss(animated: true, completion: nil)
+            }
+            alert.addAction(action)
+            present(alert, animated: true, completion: nil)
+        } else {
+            performSegue(withIdentifier: "toConfirmPage", sender: self)
+        }
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let destination = segue.destination as? ConfirmViewController{
+            destination.fullname = fullNameTextField.text
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        fullNameTextField.delegate = self
+        phoneTextField.delegate = self
     }
-
-
+    
 }
 
+extension ViewController: UITextFieldDelegate{
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if textField == fullNameTextField{
+            phoneTextField.becomeFirstResponder()
+        } else if textField == phoneTextField{
+            textField.resignFirstResponder()
+        }
+        return true
+    }
+}
